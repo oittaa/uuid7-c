5b17dffe57049c8ab143ef930b5c3f6aedc21e01
oittaa <8972248+oittaa@users.noreply.github.com>
2024-07-25 00:12:44 +0200

    Fix year 2554 problem (#9)

--- src/uuid7.c
+++ src/uuid7.c
@@ -27,32 +27,33 @@ void uuid_to_string(const uuid_t uuid, char *out) {
 
 void uuid_generate_v7(uuid_t out) {
   struct timespec tv;
-  uint64_t subsec, ts;
-  static uint64_t ts_prev = 0;
+  uint32_t subsec;
+  unsigned __int128 ts;
+  static unsigned __int128 ts_prev = 0;
   static unsigned char jitter = 0;
 
   if (getentropy(out + 9, 7) || clock_gettime(CLOCK_REALTIME, &tv))
     err(EXIT_FAILURE, NULL);
-  ts = tv.tv_sec * 1000000000ULL + tv.tv_nsec;
+  ts = (unsigned __int128)tv.tv_sec * 1000000000ULL + tv.tv_nsec;
   pthread_mutex_lock(&lock);
   if (ts <= ts_prev) {
     ts = ts_prev + 1;
-  } else if (ts - ts_prev > 1000000) {
+  } else if (ts - ts_prev > 1 << 20) {
     jitter = out[9] >> 7;
   }
   ts_prev = ts;
-  subsec = (ts % 1000000ULL << 20) / 1000000 + jitter;
+  subsec = (uint32_t)((ts % 1000000ULL << 20) / 1000000 + jitter);
   pthread_mutex_unlock(&lock);
   ts /= 1000000ULL; // unix_ts_ms
 
-  out[0] = ts >> 40;
-  out[1] = ts >> 32;
-  out[2] = ts >> 24;
-  out[3] = ts >> 16;
-  out[4] = ts >> 8;
-  out[5] = ts >> 0;
+  out[0] = ts >> 40 & 0xFF;
+  out[1] = ts >> 32 & 0xFF;
+  out[2] = ts >> 24 & 0xFF;
+  out[3] = ts >> 16 & 0xFF;
+  out[4] = ts >> 8 & 0xFF;
+  out[5] = ts & 0xFF;
   out[6] = subsec >> 16 | 0x70; // version
-  out[7] = subsec >> 8;
+  out[7] = subsec >> 8 & 0xFF;
   out[8] = (subsec >> 2 & 0x3F) | 0x80; // variant
   out[9] = (out[9] & 0x3F) | subsec << 6;
 }
