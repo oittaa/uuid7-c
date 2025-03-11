bd4a42a9e90e2671f604c6dc3ad98c5f7e1d1468
Oittaa <eero@oittaa.net>
2025-03-11 18:44:10 +0700

    gcov: Override inferred gcda/gcno files

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ gcc -Wall -Wextra -fprofile-arcs -ftest-coverage -O0 -o test_uuid7.out -I./src/ 
 gcc -Wall -Wextra -fprofile-arcs -ftest-coverage -O0 -o test_threads.out -I./src/ test_threads.c src/uuid7.c
 ./test_uuid7.out
 ./test_threads.out
-gcov -pb uuid7.c
+gcov -pb --gcno=test_uuid7.out-uuid7.gcno --gcda=test_uuid7.out-uuid7.gcda --gcno=test_threads.out-uuid7.gcno --gcda=test_threads.out-uuid7.gcda uuid7.c
